6a6af9f7d9e58ec42ed05b7e09af85df00e18278
sudhirpoojari <86469974+sudhirpoojari@users.noreply.github.com>
2026-07-28 12:32:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use('/api', billingRoutes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
